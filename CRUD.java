6490508a375f0eb36961c66d6317e99f7a0123e3
nnkkllll26 <nikelpancaran260706@gmail.com>
2026-05-14 15:13:19 +0700

    update gantti nama file

--- CRUD.java
+++ CRUD.java
@@ -31,7 +31,7 @@ class Reservasi {
 // ============================================================
 //  KELAS UTAMA: CRUD + entry point
 // ============================================================
-public class SistemRestaurant {
+public class CRUD {
 
     // ---------- Array manual sebagai penyimpanan data ----------
     static final int MAX_DATA   = 100;
